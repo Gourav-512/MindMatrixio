92f3a2b3b639cc96b6af0ce4d7f35dd8194ccd7e
Gourav Salunkhe <137747550+Gourav-512@users.noreply.github.com>
2026-06-15 17:54:40 +0530

    Update db.php

--- db.php
+++ db.php
@@ -5,6 +5,7 @@ $username = "root"; // MYSQLUSER
 $password = "rKfNzrLnmlYjwyObHAsBlyLsSVLqMOvP";
 $port = 3306; // MYSQLPORT
 
+
 try {
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
