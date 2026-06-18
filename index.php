e61664a306afbc0511a818ef9d185d361c3c359d
Gourav Salunkhe <137747550+Gourav-512@users.noreply.github.com>
2026-06-18 23:23:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ include 'db.php';
     <link rel="stylesheet" type="text/css" href="assets/css/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
+    
     <style>
         /* Index Page - Professional Design */
 
