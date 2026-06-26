d52349eb3a747d74cb38486be4b8569450bf0c2d
Gourav Salunkhe <137747550+Gourav-512@users.noreply.github.com>
2026-06-26 23:56:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <?php
 header("Location: php/login.php");
 exit();
-
 // If user is already logged in, redirect to services page
 if (isset($_SESSION['user_id'])) {
     header("Location: services.php");
