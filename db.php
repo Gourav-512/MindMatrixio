ad3ed310906970dd37e1d96b9c68b3a33f4d720c
Gourav Salunkhe <137747550+Gourav-512@users.noreply.github.com>
2026-07-02 00:53:24 +0530

    Fix database connection parameters in db.php

--- db.php
+++ db.php
@@ -4,6 +4,7 @@ $dbname = "railway"; // MYSQLDATABASE
 $username = "root"; // MYSQLUSER
 $password = "rKfNzrLnmlYjwyObHAsBlyLsSVLqMOvP";
 $port = 3306; // MYSQLPORT
+
 try {
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
